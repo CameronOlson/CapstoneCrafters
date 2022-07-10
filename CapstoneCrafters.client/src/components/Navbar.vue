2634238510757cd7735d39df5488cd9d529db007
CameronOlson <olsoncam10@gmail.com>
2022-07-10 14:38:52 -0600

    blocking the Home Page

--- CapstoneCrafters.client/src/components/Navbar.vue
+++ CapstoneCrafters.client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <h5>Capstone Crafters</h5>
       </div>
     </router-link>
     <button
@@ -19,16 +19,14 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link
+          <!-- <router-link
             :to="{ name: 'About' }"
             class="btn text-success lighten-30 selectable text-uppercase"
           >
             About
-          </router-link>
+          </router-link> -->
         </li>
       </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <Login />
     </div>
   </nav>
 </template>
---
+++ CapstoneCrafters.client/src/pages/Account1Page.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="component">This is Account 1 profile page</div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
